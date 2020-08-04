35d713cafaef4b86a81e9d4940f08e0d3187abef
Winton DeShong <contact@winton.me>
2020-08-04 14:53:11 -0400

    UserLogins#Create integrations tests

--- dotnet/api/Tests/Presentation/Web.Tests/Integration/Controllers/Api/V1/UserLogins/UserLoginsControllerTest.cs
+++ dotnet/api/Tests/Presentation/Web.Tests/Integration/Controllers/Api/V1/UserLogins/UserLoginsControllerTest.cs
@@ -9,6 +9,10 @@ using Testing.Constants;
 using LMS.Presentation.Web.Controllers.Api.V1.UserLogins;
 using AndcultureCode.GB.Presentation.Web.Models.Dtos.Users;
 using System.Linq;
+using AndcultureCode.GB.Business.Core.Models.Entities.Users;
+using AndcultureCode.GB.Business.Core.Interfaces.Conductors.Domain.Users;
+using AndcultureCode.CSharp.Testing.Extensions;
+using AndcultureCode.GB.Business.Conductors.Domain.UserLogins;
 
 namespace AndcultureCode.GB.Presentation.Web.Tests.Integration.Controllers.Api.V1.Roles
 {
@@ -35,13 +39,24 @@ namespace AndcultureCode.GB.Presentation.Web.Tests.Integration.Controllers.Api.V
 
         #region Create
 
+        private User CreateUser(string password)
+        {
+            var user = Build<User>();
+
+            GetDep<IUserLoginConductor<User>>().SetPassword(user, password);
+            GBApiContext.Users.Add(user);
+            GBApiContext.SaveChanges();
+
+            return user;
+        }
+
         [Fact]
         public void Create_When_UserName_DoesNotExist_Then_Creates_Failed_Login_And_Returns_BadRequest()
         {
             // Arrange
             var dto = new UserLoginDto
             {
-                UserName = "imaginary-user"
+                UserName = "imaginary-user" // <--------------------
             };
 
             // Act
@@ -51,6 +66,49 @@ namespace AndcultureCode.GB.Presentation.Web.Tests.Integration.Controllers.Api.V
             var userLogin = GBApiContext.UserLogins.FirstOrDefault();
             userLogin.ShouldNotBeNull();
             userLogin.IsSuccessful.ShouldBeFalse();
+            result.ShouldHaveErrorsFor(UserLoginConductor<User>.ERROR_INVALID_CREDENTIALS);
+        }
+
+        [Fact]
+        public void Create_When_UserName_Exists_When_Incorrect_Password_Then_Creates_Failed_Login_And_Returns_BadRequest()
+        {
+            // Arrange
+            var user = CreateUser(password: "actual-password");
+            var dto = new UserLoginDto
+            {
+                Password = "incorrect-password", // <--------------------
+                UserName = user.UserName
+            };
+
+            // Act
+            var result = Sut.Create(dto).AsBadRequest<Result<Object>>();
+
+            // Assert
+            var userLogin = GBApiContext.UserLogins.FirstOrDefault();
+            userLogin.ShouldNotBeNull();
+            userLogin.IsSuccessful.ShouldBeFalse();
+            result.ShouldHaveErrorsFor(UserLoginConductor<User>.ERROR_INVALID_CREDENTIALS);
+        }
+
+        [Fact]
+        public void Create_When_UserName_And_Password_Correct_Then_Creates_Successful_Login_And_Returns_Created()
+        {
+            // Arrange
+            var password = "password";
+            var user = CreateUser(password);
+            var dto = new UserLoginDto
+            {
+                Password = password,
+                UserName = user.UserName
+            };
+
+            // Act
+            var result = Sut.Create(dto).Result.AsCreated<Result<UserLoginDto>>();
+
+            // Assert
+            var userLogin = GBApiContext.UserLogins.FirstOrDefault();
+            userLogin.ShouldNotBeNull();
+            userLogin.IsSuccessful.ShouldBeTrue();
         }
 
         #endregion Create

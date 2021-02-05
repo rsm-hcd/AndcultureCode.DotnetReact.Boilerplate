2971e78b9641968d2a56db53c93c1872e70ea6fc
Dylan Justice <dylanjustice41@gmail.com>
2021-02-04 20:43:15 -0500

    Upgrading cypress to latest version
    Per documenation, removing customized matchers. It's best to stay as
    close to the base implementation of cypress as possible.

--- frontend/cypress/matchers/element-matchers.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="Cypress" />
-
-/**
- * Assert that an element exists on the page
- * @param selector
- */
-export const shouldExist = (selector) => cy.get(selector).should("exist");

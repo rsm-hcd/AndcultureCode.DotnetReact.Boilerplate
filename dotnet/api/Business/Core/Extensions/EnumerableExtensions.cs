25a3d3a0702dc737a789127719cf34a3f8dc189e
Jon Dahlin <E090511@mcgladrey.rsm.net>
2022-06-23 10:39:01 -0500

    upgrading to .net 6

--- dotnet/api/Business/Core/Extensions/EnumerableExtensions.cs
+++ dotnet/api/Business/Core/Extensions/EnumerableExtensions.cs
@@ -8,16 +8,7 @@ namespace AndcultureCode.GB.Business.Core.Extensions
     // TODO: Move to AndcultureCode.CSharp.Extensions
     public static class EnumerableExtensions
     {
-        /// <summary>
-        /// Returns a distinct enumerable by a specific property
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TKey"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="property"></param>
-        /// <returns></returns>
-        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> property)
-            => source.GroupBy(property).Select(x => x.First());
+        
 
         /// <summary>
         /// Returns a distinct list by a specific property
